e2b0c293d01ef56b63284b9ee0ee682dc68a3941
David Gavino <ggavino2@uccs.edu>
2021-11-13 18:26:02 -0700

    added qustions to js file

--- Assets/style.js
+++ Assets/style.js
@@ -1,38 +1,46 @@
 
 var count = 15;
-var interval = setInterval(function () {
-    document.getElementById('count').innerHTML = count;
-    count--;
-    if (count === 0) {
-        clearInterval(interval);
-        document.getElementById('count').innerHTML = 'Done';
-        // or...
-        alert("You're out of time!");
-    }
-}, 1000);
+// var interval = setInterval(function () {
+//     document.getElementById('count').innerHTML = count;
+//     count--;
+//     if (count === 0) {
+//         clearInterval(interval);
+//         document.getElementById('count').innerHTML = 'Done';
+//         // or...
+//         alert("You're out of time!");
+//     }
+// }, 1000);
 
-// Entering comments is a useless task, it will not help in anyway.
-// True 
-// False *
+var questions = [
+{
+question: "Entering comments is a useless task, it will not help in anyway.",
+choices: ["True", "False"], 
+Answer: "False"
+},
 
-// A short section of code written to cpmplete a task.
-// Buffer
-// Array
-// Function *
+{
+    question: "A short section of code written to cpmplete a task." ,
+    choices: ["Buffer", "Array", "Function"],
+    answer: "Function"
+},
 
-// A syntax error is?
-// An error due to user error.
-// An error caused by language rules being broken.*
-// An error you will never find.
+{
+    question: "A syntax error is?",
+    choices: ["An error due to user error.", 
+    "An error caused by language rules being broken.", 
+    "An error you will never find."],
+    answer: "An error caused by language rules being broken."
+},
 
-// What does this equation mean? a != T
-// a is assigned to T
-// a and T are equal.
-// a is not equal to T.*
-// T is added to a.
+{
+    question: "What does this equation mean? a != T",
+    choices: ["a is assigned to T", "a and T are equal", "a is not equal to T", "T is added to a"],
+    answer: "a is not equal to T"
+},
 
-//Arrays in Javascript can be used to store?
-// numbers and strings
-// other arrays
-// booleans
-// All of the above*
+{
+    question: "Arrays in Javascript can be used to store?",
+    choices: ["numbers and strings", "other arrays", "booleans", "All of the above"],
+    answer: "All of the above"
+}
+]
